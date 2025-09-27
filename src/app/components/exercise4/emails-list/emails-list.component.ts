14e4611ed186f5c21ed42969315bec510cac1b08
DavveDP <dgriley02@gmail.com>
2025-09-27 23:28:49 +0200

    Add filters

--- src/app/components/exercise4/emails-list/emails-list.component.ts
+++ src/app/components/exercise4/emails-list/emails-list.component.ts
@@ -2,16 +2,22 @@ import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { EmailService } from '../../../services/email-service.service';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { FilterContentPipe } from '../../../pipes/filter-content.pipe';
+import { WithBodyPipe } from '../../../pipes/with-body.pipe';
 
 @Component({
   selector: 'app-emails-list',
   standalone: true,
-  imports: [NgFor, NgIf, RouterLink],
+  imports: [FormsModule, NgFor, NgIf, RouterLink, FilterContentPipe, WithBodyPipe],
   templateUrl: './emails-list.component.html',
   styleUrl: './emails-list.component.css'
 })
 export class EmailsListComponent {
   constructor(public emailService: EmailService){}
+
+  filterStr: string = "";
+  withBodyChecked: boolean = false;
 
   deleteEmail(id: number): void {
     if(!this.emailService.delete(id)) {
